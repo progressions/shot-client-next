53974dcea75dc108d2d2a91f12da336c0a8d0dcb
Isaac Priestley <progressions@gmail.com>
2025-07-25 08:31:04 -0400

    Add loading

---
+++ src/app/loading.tsx
@@ -0,0 +1,20 @@
+import { Box, CircularProgress, Typography } from "@mui/material"
+
+export default function Loading() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      <CircularProgress />
+      <Typography variant="h6" sx={{ mt: 2 }}>
+        Loading...
+      </Typography>
+    </Box>
+  )
+}
