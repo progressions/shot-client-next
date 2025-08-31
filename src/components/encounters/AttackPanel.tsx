992f6ba4feeede55d79b75faa03b6433ed60f979
Isaac Priestley <progressions@gmail.com>
2025-08-31 13:17:10 -0400

    Fix ReferenceError by removing non-existent refreshEncounter call

--- src/components/encounters/AttackPanel.tsx
+++ src/components/encounters/AttackPanel.tsx
@@ -372,7 +372,6 @@ export default function AttackPanel({ onClose }: AttackPanelProps) {
       )
 
       toastSuccess(`Applied ${damage} damage to ${target.name}`)
-      await refreshEncounter()
 
       // Reset form
       setTargetShotId("")
