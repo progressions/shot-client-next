0d294972d82089bcb99a11817a3ea99e72541e25
Isaac Priestley <git@progressions.me>
2025-11-17 11:17:42 -0500

    chore: Remove debug logging from UploadImageDialog

--- src/components/generate/UploadImageDialog.tsx
+++ src/components/generate/UploadImageDialog.tsx
@@ -169,8 +169,6 @@ export function UploadImageDialog({
             entity.id,
             formData
           )
-          console.log("Upload response:", response.data)
-          console.log("Image URL from response:", response.data.image_url)
           setEntity?.(response.data)
           const imageUrl = response.data.image_url
           dispatchForm({
