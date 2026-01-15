caad1ed4be2e70f23a4bdb8def417ff4fb03897e
Isaac Priestley <progressions@gmail.com>
2026-01-15 10:48:36 -0500

    chore: Remove unused *Mobile components
    Remove dead code that was replaced by GridView pattern:
    - AdventuresMobile.tsx
    - CharactersMobile.tsx
    - FightsMobile.tsx
    - SitesMobile.tsx
    
    All entities now consistently use GridView with DetailComponent for mobile view.

--- src/components/characters/index.ts
+++ src/components/characters/index.ts
@@ -1,6 +1,5 @@
 export { default as CharacterName } from "@/components/characters/CharacterName"
 export { default as List } from "@/components/characters/List"
-export { default as CharactersMobile } from "@/components/characters/CharactersMobile"
 export { default as CharacterPageClient } from "@/components/characters/CharacterPageClient"
 export { default as ImportPage } from "@/components/characters/ImportPage"
 export { default as GeneratePage } from "@/components/characters/GeneratePage"
